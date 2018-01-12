1f2f522de019b6aca5630c4f600084f938925a4a
Alessia Nigretti <alessian@unity3d.com>
2018-01-12 15:43:09 +0000

    Uploaded project

---
+++ Assets/Scripts/RestartTimeline.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class RestartTimeline : MonoBehaviour
+{
+	public PlayableDirector timeline;
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.R))
+		{
+			timeline.time = 0;
+		}
+	}
+}
